652a6e8becd6c2fa632f6f2cc11f7cdfd2cef259
Jeykym <jachym.sulman@gmail.com>
2026-04-28 20:40:29 +0200

    add a PropertyTag model

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -32,6 +32,7 @@ class EstateProperty(models.Model):
         ],
         default='new'
     )
-    property_type_id = fields.Many2one("estate.property.type", string="Type")
     seller_id = fields.Many2one("res.users", string="Seller", default=lambda self: self.env.user)
     buyer_id = fields.Many2one("res.partner", string="Buyer")
+    property_type_id = fields.Many2one("estate.property.type", string="Type")
+    property_tag_id = fields.Many2many("estate.property.tag", string="Tags")
