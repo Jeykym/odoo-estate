521de88d7de0effa6230ed66aeb949ca94b990b1
Jeykym <jachym.sulman@gmail.com>
2026-04-28 20:31:22 +0200

    add seller and buyer properties to EstateProperty

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -33,3 +33,5 @@ class EstateProperty(models.Model):
         default='new'
     )
     property_type_id = fields.Many2one("estate.property.type", string="Type")
+    seller_id = fields.Many2one("res.users", string="Seller", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="Buyer")
