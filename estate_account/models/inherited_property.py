d979626d06570dac4217b04a4f8a7b2548d226e4
Jáchym Šulman <jachym.sulman@gmail.com>
2026-05-11 20:21:43 +0200

    add invoice line fees to sold property invoice

--- estate_account/models/inherited_property.py
+++ estate_account/models/inherited_property.py
@@ -1,8 +1,27 @@
-from odoo import models, fields
+from odoo import models, fields, Command
 
 class InheritedProperty(models.Model):
     _inherit = "estate.property"
 
     def action_sold(self):
-        print("Inherited sold action called")
-        return super().action_sold()
+        res = super().action_sold()
+
+        for record in self:
+           self.env['account.move'].create({
+               'move_type': 'out_invoice',
+               'partner_id': record.buyer_id.id,
+               'line_ids': [
+                    Command.create({
+                        'name': 'Commision fee',
+                        'quantity': 1,
+                        'price_unit': record.selling_price * 0.06
+                    }),
+                    Command.create({
+                        'name': 'Administration fee',
+                        'quantity': 1,
+                        'price_unit': 100
+                    })
+               ]
+           }) 
+
+        return res
